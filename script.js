4eac138335fef2468ecdc524e0011ce895a06bcd
Zachary Binx <72980919+ZacharyBinx@users.noreply.github.com>
2020-11-11 14:42:44 -0700

    css styled, still working on functions for leaderboard

--- script.js
+++ script.js
@@ -14,39 +14,39 @@ let timerOn = true;
 
 const questions = [
     {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
+        text: "Which famous twitch streamer succeeded in a No-Hit Run in Resident Evil 2 in relation to a bet about shaving his/her head?",
+        answers: ["Cahlaflour", "Fruitbats", "The_Happy_Hob"],
+        correctIndex: 2,
+    },
+    {
+        text: "If attacked by a zombie, which virus do you contract?",
+        answers: ["T-Virus", "Z-Virus", "Wombo-Zombo-Combo"],
         correctIndex: 0,
     },
     {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
+        text: "What was the original name for 'Resident Evil' before coming to America?",
+        answers: ["The Virus", "Infected", "Biohazard"],
+        correctIndex: 2,
+    },
+    {
+        text: "When was the zombie 'Nemesis' introduced to the series?",
+        answers: ["Resident Evil: 0", "Resident Evil 3", "Resident Evil"],
+        correctIndex: 1,
+    },
+    {
+        text: "You were able to choose 1 of 2 characters in Resident Evil. What were their names?",
+        answers: ["Rebecca Chambers and Billy Coen", "Leon Kennedy and Jill Valentine", "Jill Valentine and Chris Redfield"],
+        correctIndex: 2,
+    },
+    {
+        text: "What is the name of the corporation responsible for the outbreak of the virus?",
+        answers: ["Umbrella", "Tricell", "MedSmart"],
         correctIndex: 0,
     },
     {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
-        correctIndex: 0,
-    },
-    {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
-        correctIndex: 0,
-    },
-    {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
-        correctIndex: 0,
-    },
-    {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
-        correctIndex: 0,
-    },
-    {
-        text: "question?",
-        answers: ["answer 1", "answer 2", "answer 2"],
-        correctIndex: 0,
+        text: "Name the creepy crawly which sensed your movements in the Resident Evil series!",
+        answers: ["Crawler", "Hook Man", "Licker"],
+        correctIndex: 2,
     },
     {
         text: "question?",
@@ -66,14 +66,14 @@ const questions = [
 ];
 let questionIndex = 0;
 
-if(startBtn){
+if (startBtn) {
     startBtn.addEventListener("click", handleStartClick);
 }
-if(answersDiv){
-answersDiv.addEventListener("click", handleAnswerClick);
+if (answersDiv) {
+    answersDiv.addEventListener("click", handleAnswerClick);
 }
-if(scoreSubmit){
-scoreSubmit.addEventListener("click", subUserScore);
+if (scoreSubmit) {
+    scoreSubmit.addEventListener("click", subUserScore);
 }
 
 function handleStartClick(e) {
@@ -126,7 +126,7 @@ function renderQuestion() {
 
         const btn = document.createElement("button");
 
-        btn.setAttribute("class", "btn btn-primary");
+        btn.setAttribute("class", "btn btn-text border border-danger");
         btn.textContent = answer;
         answersDiv.appendChild(btn);
     }
@@ -139,8 +139,8 @@ function healthStart() {
     var timeInterval = setInterval(function () {
 
         timerEl.textContent = "Health: " + timeLeft;
-        if (timerOn){
-        timeLeft--;
+        if (timerOn) {
+            timeLeft--;
         }
         if (timeLeft <= 0 || !timerOn) {
             clearInterval(timeInterval);
@@ -168,15 +168,13 @@ function subUserScore(e) {
     const highScore = JSON.stringify(score);
 
     localStorage.setItem("finalscore", highScore);
-    console.log(highScore);
-    console.log(userInitials);
-   
+
+    window.location.replace("score.html")
     renderScoreBoard();
 }
 
-function renderScoreBoard(){
-    window.location.replace("score.html")
-    let storedUser = localStorage.getItem("User");
+function renderScoreBoard() {
+    let storedUser = window.localStorage.getItem("User");
     let storedScore = localStorage.getItem("finalscore");
 
     if (storedUser) {
@@ -185,6 +183,25 @@ function renderScoreBoard(){
     if (storedScore) {
         userScoreFinal = JSON.parse(storedScore);
     }
-    console.log(userScoreFinal);
+
     console.log(userInitFinal);
+    console.log(userScoreFinal);
+
+    highScores.sort(function (a, b) {
+        return b.userscore - a.userscore;
+    });
+
+    for (let i = 0; i < highScores.length; i++) {
+
+        var row = document.createElement("tr");
+        for (let i = 0; i < highScores.length; i++) {
+            const table = document.querySelector("#leaderboard");
+            var cell = document.createElement("td");
+            var userText = document.createTextNode(userInitFinal, userScoreFinal);
+            cell.appendChild(userText);
+            row.appendChild(cell);
+            table.appendChild(row);
+        }
+    }
+
 }
